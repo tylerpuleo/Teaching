21ae21649a4331c1387af9c4af52beee391d37a5
Tyler <tyler.puleo22@gmail.com>
2014-10-20 14:30:56 -0400

    added vb project for running vbscript

---
+++ ExampleCode/Visual Basic/RunVBscript/RunVBscript/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim wsh
+        wsh = CreateObject("WScript.Shell")
+        wsh.run("C:\Users\tylerpuleo\Documents\VBSCRIPT\VbsJson.vbs", 1, 0)
+    End Sub
+End Class
