f3a59b1c47d688eb6481ad1944a0273787e7f57f
Tyler <tyler.puleo22@gmail.com>
2014-10-21 16:29:16 -0400

    Changed path of VB to git folder

--- ExampleCode/VisualBasic/RunVBscript/RunVBscript/Form1.vb
+++ ExampleCode/VisualBasic/RunVBscript/RunVBscript/Form1.vb
@@ -3,6 +3,6 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim wsh
         wsh = CreateObject("WScript.Shell")
-        wsh.run("C:\Users\tylerpuleo\Documents\VBSCRIPT\VbsJson.vbs", 1, 0)
+        wsh.run("C:\Users\tylerpuleo\Documents\Teaching\ExampleCode\VisualBasic\VbsJson.vbs", 1, 0)
     End Sub
 End Class
